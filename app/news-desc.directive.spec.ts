437256fd17d77e922c93e1cd9cbe51870a1ed088
kskarthikumar <39190993+kskarthikumar@users.noreply.github.com>
2018-06-13 12:51:27 +0530

    Create news-desc.directive.spec.ts

---
+++ app/news-desc.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NewsDescDirective } from './news-desc.directive';
+
+describe('NewsDescDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NewsDescDirective();
+    expect(directive).toBeTruthy();
+  });
+});
